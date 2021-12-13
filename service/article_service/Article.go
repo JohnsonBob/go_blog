2960bb3ba1c6f7f42fdee3f83805371d80df20e6
Johnson <poiuytrengo@qq.com>
2021-12-13 23:53:53 +0800

    1.update

--- service/article_service/Article.go
+++ service/article_service/Article.go
@@ -25,6 +25,23 @@ func GetOne(id int) (article *models.Article, err error) {
 	return
 }
 
+func GetAll(article *cache_service.Article) (articles *[]models.Article, err error) {
+	key := article.GetArticleKey()
+	if gredis.IsExists(key) {
+		var data []byte
+		data, err = gredis.Get(key)
+		if err != nil {
+			return
+		}
+		err = json.Unmarshal(data, &articles)
+		return
+	}
+
+	*articles = models.GetArticles(article.PageNum, article.PageSize, article)
+	err = gredis.Set(key, article, 1200)
+	return
+}
+
 //func ExistArticleById(id int) (bool, err error) {
 //	cacheArticle := cache_service.Article{ID: id}
 //	key := cacheArticle.GetArticleKey()
